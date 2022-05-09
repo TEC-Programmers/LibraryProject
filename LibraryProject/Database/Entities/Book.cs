c4dc97c2b3402af73d8a9270cdd5b90e9591bdbe
Afrinarahaman <70579511+Afrinarahaman@users.noreply.github.com>
2022-05-09 08:27:08 +0200

    Made Book, Category classes

---
+++ LibraryProject/Database/Entities/Book.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryProject.Database.Entities
+{
+    public class Book
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(32)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(32)")]
+        public decimal Language { get; set; }
+
+        [Column(TypeName = "nvarchar(32)")]
+        public string Description { get; set; }
+
+        [Column(TypeName = "nvarchar(32)")]
+        public string PublishYear { get; set; }
+
+        public int CategoryId { get; set; }
+        
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public int AuthorId { get; set; }
+
+        [ForeignKey("AuhtorId")]
+        public Author Author { get; set; }
+
+        public int PublisherId { get; set; }
+
+        [ForeignKey("PublisherId")]
+        public Publisher  Publisher{ get; set; }
+
+
+    }
+}
