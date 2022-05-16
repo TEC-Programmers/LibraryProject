8eb1d0a55e1c41d6aab4888b3f0bdb28636cf62f
Afrinarahaman <70579511+Afrinarahaman@users.noreply.github.com>
2022-05-16 12:59:36 +0200

    Worked with Book Repository Tests

--- LibraryProject.Tests/Repositories/BookRepositoryTests.cs
+++ LibraryProject.Tests/Repositories/BookRepositoryTests.cs
@@ -87,6 +87,292 @@ namespace LibraryProject.Tests.Repositories
             Assert.Equal(2, result.Count);
             // Assert.Empty(result);
         }
+        [Fact]
+        public async void SelectAllBooks_ShouldReturnEmptyListOfBooks_WhenNoBookExists()
+        {
+            //Arrange 
+            await _context.Database.EnsureDeletedAsync();
 
+
+
+
+            //Act
+            var result = await _bookRepository.SelectAllBooks();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<Book>>(result);
+
+            Assert.Empty(result);
+        }
+        [Fact]
+        public async void SelectBookById_ShouldReturnBook_WhenBookExists()
+        {
+            //Arrange 
+            await _context.Database.EnsureDeletedAsync();
+            int bookId = 1;
+            _context.Category.Add(new()
+            {
+                Id = 1,
+                CategoryName = "Børnebog"
+
+            });
+            _context.Author.Add(new()
+            {
+                Id = 1,
+                FirstName = "Astrid",
+                MiddleName = "",
+                LastName = " Lindgrens"
+
+            });
+
+            _context.Book.Add(new()
+            {
+
+                Id = 1,
+                Title = "Pipi Langstrømper",
+                Description = "Kids bog ",
+                Language = "Dansk",
+                PublishYear = 1945,
+                CategoryId = 1,
+                AuthorId = 1
+
+
+
+            });
+
+
+            await _context.SaveChangesAsync();
+
+            //Act
+            var result = await _bookRepository.SelectBookById(bookId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Book>(result);
+            Assert.Equal(bookId, result.Id);
+            // Assert.Empty(result);
+        }
+        [Fact]
+        public async void SelectBookById_ShouldReturnNull_WhenBookDoesNotExist()
+        {
+            //Arrange 
+            await _context.Database.EnsureDeletedAsync();
+
+
+
+
+            //Act
+            var result = await _bookRepository.SelectBookById(1);
+
+            //Assert
+
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void InsertNewBook_ShouldAddnewIdToBook_WhenSavingToDatabase()
+        {
+            //Arrange 
+            await _context.Database.EnsureDeletedAsync();
+
+            int expectedNewId = 1;
+
+            Book book = new()
+            {
+                Id = 1,
+                Title = "Pipi Langstrømper",
+                Description = "Kids bog ",
+                Language = "Dansk",
+                PublishYear = 1945,
+                CategoryId = 1,
+                AuthorId = 1
+
+
+
+            };
+
+
+            await _context.SaveChangesAsync();
+
+            //Act
+            var result = await _bookRepository.InsertNewBook(book);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Book>(result);
+            Assert.Equal(expectedNewId, result.Id);
+
+        }
+
+        [Fact]
+        public async void InsertNewProduct_ShouldFailToAddNewProduct_WhenProductIdAlreadyExists()
+        {
+            //Arrange 
+            await _context.Database.EnsureDeletedAsync();
+
+
+
+            Book book = new()
+            {
+                Id = 1,
+                Title = "Pipi Langstrømper",
+                Description = "Kids bog ",
+                Language = "Dansk",
+                PublishYear = 1945,
+                CategoryId = 1,
+                AuthorId = 1
+            };
+
+            _context.Book.Add(book);
+            await _context.SaveChangesAsync();
+
+            //Act
+            async Task action() => await _bookRepository.InsertNewBook(book);
+
+
+            //Assert
+            var ex = await Assert.ThrowsAsync<ArgumentException>(action);
+            Assert.Contains("An item with the same key has already been added", ex.Message);
+
+        }
+
+        [Fact]
+        public async void UpdateExistingBook_ShouldChangeValuesOnBook_WhenBookExists()
+        {
+            //Arrange 
+            await _context.Database.EnsureDeletedAsync();
+            int bookId = 1;
+          
+            Book newBook = new()
+            {
+                Id = 1,
+                Title = "Pipi Langstrømper",
+                Description = "Kids bog ",
+                Language = "Dansk",
+                PublishYear = 1945,
+                CategoryId = 1,
+                AuthorId = 1
+
+
+            };
+
+            _context.Book.Add(newBook);
+            await _context.SaveChangesAsync();
+
+            Book updateBook = new()
+            {
+                Id = bookId,
+                Title = "Updated Pipi Langstrømper",
+                Description = " updated Kids bog ",
+                Language = " updatedDansk",
+                PublishYear = 1945,
+                CategoryId = 1,
+                AuthorId = 1
+
+            };
+
+
+
+            //Act
+            var result = await _bookRepository.UpdateExistingBook(bookId, updateBook);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Book>(result);
+            Assert.Equal(bookId, result.Id);
+            Assert.Equal(updateBook.Title, result.Title);
+            Assert.Equal(updateBook.Language, result.Language);
+            Assert.Equal(updateBook.Description, result.Description);
+            Assert.Equal(updateBook.PublishYear, result.PublishYear);
+            Assert.Equal(updateBook.CategoryId, result.CategoryId);
+            Assert.Equal(updateBook.AuthorId, result.AuthorId);
+
+
+        }
+
+        [Fact]
+        public async void UpdateExistingBook_ShouldReturnNull_WhenBookDoesNotExist()
+        {
+            //Arrange 
+            await _context.Database.EnsureDeletedAsync();
+            int bookId = 1;
+
+
+            Book updateBook = new()
+            {
+                Id = bookId,
+                Title = "Updated Pipi Langstrømper",
+                Description = " updated Kids bog ",
+                Language = " updatedDansk",
+                PublishYear = 1945,
+                CategoryId = 1,
+                AuthorId = 1
+
+            };
+
+
+
+            //Act
+            var result = await _bookRepository.UpdateExistingBook(bookId, updateBook);
+
+            //Assert
+            Assert.Null(result);
+
+        }
+
+        [Fact]
+        public async void DeleteBookById_ShouldReturnDeleteBook_WhenBookIsDeleted()
+        {
+            //Arrange 
+            await _context.Database.EnsureDeletedAsync();
+            int bookId = 1;
+            Book newBook = new()
+            {
+                Id = bookId,
+                Title = "Updated Pipi Langstrømper",
+                Description = " updated Kids bog ",
+                Language = " updatedDansk",
+                PublishYear = 1945,
+                CategoryId = 1,
+                AuthorId = 1
+
+            };
+
+            _context.Book.Add(newBook);
+            await _context.SaveChangesAsync();
+
+
+
+
+            //Act
+            var result = await _bookRepository.DeleteBookById(bookId);
+            var book = await _bookRepository.SelectBookById(bookId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Book>(result);
+            Assert.Equal(bookId, result.Id);
+            Assert.Null(book);
+        }
+        [Fact]
+        public async void DeleteBookById_ShouldReturnNull_WhenBookDoesNotExist()
+        {
+            //Arrange 
+            await _context.Database.EnsureDeletedAsync();
+
+
+
+
+
+            //Act
+            var result = await _bookRepository.DeleteBookById(1);
+
+
+            //Assert
+
+            Assert.Null(result);
+        }
     }
 }
