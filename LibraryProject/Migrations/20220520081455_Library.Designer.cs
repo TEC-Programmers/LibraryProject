5525b17748030977d3723184892aef0e9ba5d53c
Azam Fahim <azam0001@elev.tec.dk>
2022-05-20 13:54:23 +0200

    changes

--- LibraryProject/Migrations/20220520081455_Library.Designer.cs
+++ LibraryProject/Migrations/20220520081455_Library.Designer.cs
@@ -12,7 +12,7 @@ namespace LibraryProject.API.Migrations
     [Migration("20220520081455_Library")]
     partial class Library
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
