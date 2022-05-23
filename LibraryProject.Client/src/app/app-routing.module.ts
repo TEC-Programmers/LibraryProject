77fe380f04d342ddafca6ed7d978606ca2a974d3
BILALBEAST <81167123+BILALBEAST@users.noreply.github.com>
2022-05-23 10:39:57 +0200

    Made new changes to searchbar

--- LibraryProject.Client/src/app/app-routing.module.ts
+++ LibraryProject.Client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthorComponent } from './author/author.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 
 const routes: Routes = [
   {path: '', component : FrontpageComponent},
-  {path: 'Author', component:AuthorComponent}
+  {path: 'Author', component:AuthorComponent},
+  {path: 'Book/:id', component: FrontpageComponent},
+  {path: 'Category/:id', component: FrontpageComponent}
 ];
 
 @NgModule({
--- LibraryProject.Client/src/app/app.component.ts
+++ LibraryProject.Client/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { Book } from './_models/Book';
+import { Category } from './_models/Category';
+import { BookService } from './_services/book.service';
+import { CategoryService } from './_services/category.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +12,35 @@ import { RouterModule } from '@angular/router';
 })
 export class AppComponent {
   title = 'LibraryProject-Client';
+  book!: Book;
+  counter = 0;
+  total: number = 0;
+  categorys: Category[] = [];
+
+  allBooks: Book[] = [];
+  filterTerm!: string;
+
+
+  constructor(private bookService: BookService, category: CategoryService) {}
+  showSearch(): void {
+
+    if (this.filterTerm == null || this.filterTerm == '') {
+     alert("The input field is empty")
+    }
+
+    else if (this.filterTerm.length >= 0 ){
+      this.bookService.getAllBooks()
+    .subscribe(p => this.allBooks = p);
+    console.log(this.allBooks)
+    }
+      
+    
+  }
+    checkSearch(event:any){
+      if (event.key === "Backspace" || this.filterTerm == null) {
+        this.allBooks = [];
+        console.log(event);
+      
+      }
+    }
 }
