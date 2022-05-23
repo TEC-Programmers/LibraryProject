c69d4c9518ab427b9d305643861524a320224e88
Azam Fahim <azam0001@elev.tec.dk>
2022-05-23 09:58:02 +0200

    save changes. Category routerlink

--- LibraryProject.Client/src/app/app.component.ts
+++ LibraryProject.Client/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CategoryComponent } from './category/category.component';
+import { Author } from './_models/Author';
+import { Category } from './_models/Category';
 
 
 @Component({
@@ -9,12 +12,37 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'LibraryProject-Client';
 
+  catehory: Category[] = []
+
+  author: Author[] = []
+
+  authors: Author = {
+    id: 0,
+    firstName: '',
+    lastName: ''
+  }
 
   public categories = ["børnebøger","voksenbøger", "Manga",]
+  
 
   constructor() {}
 
   ngOnInit(): void {
+    this.authors =  {
+      id: 1,
+      firstName: 'Bilal',
+      lastName: '16'
+    },
+    {
+      id: 2,
+      firstName: 'Azam',
+      lastName: '18'
+    },
+    {
+      id: 3,
+      firstName: 'Mahmood',
+      lastName: '22'
+    };
   }
 
 }
