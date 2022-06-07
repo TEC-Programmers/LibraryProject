1dfba2c9ca8c6d862d96c360aca8fef77b3e25f0
Rizwanah Mustafa <rizw0048@elev.tec.dk>
2022-06-07 10:04:38 +0200

    Register Component er færdigt

--- LibraryProject.Client/src/app/_services/user.service.ts
+++ LibraryProject.Client/src/app/_services/user.service.ts
@@ -29,7 +29,7 @@ export class UserService {
   }
 
   registerUser(user: User): Observable<User>{
-    return this.http.post<User>(this.apiUrl, user, this.httpOptions);
+    return this.http.post<User>(this.apiUrl + `/register`, user, this.httpOptions);
   }
 
   updateUser(id: number, user:User): Observable<User> {
