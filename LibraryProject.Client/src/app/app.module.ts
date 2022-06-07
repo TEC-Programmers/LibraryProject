94be7acee6d6a632c824539da2201b52b26aedad
Fahim <Azmudhim@gmail.com>
2022-06-07 14:06:31 +0200

    .

--- LibraryProject.Client/src/app/app.module.ts
+++ LibraryProject.Client/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { FormsModule } from '@angular/forms';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { AdministratorComponent } from './administrator/administrator.component';
 import { AdminCustomerComponent } from './admin-customer/admin-customer.component';
-
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { DatePipe } from '@angular/common';
 import { CategoryComponent } from './category/category.component';
