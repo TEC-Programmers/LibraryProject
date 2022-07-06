37b880d87e53aecfb5b84d9952bd39c963f3b7b9
Afrinarahaman <70579511+Afrinarahaman@users.noreply.github.com>
2022-07-06 14:14:14 +0200

    some changes

--- LibraryProject.Client/src/app/app.component.ts
+++ LibraryProject.Client/src/app/app.component.ts
@@ -22,23 +22,24 @@ export class AppComponent {
   allBooks: Book[] = [];
   filterTerm!: string;
   searchBooks: Book[] = [];
-  currentUser: User ={ id: 0, firstName: '', middleName: '', lastName: '', email: '', password: ''};
-  public searchTerm:string="";
+  currentUser: User = { id: 0, firstName: '', middleName: '', lastName: '', email: '', password: '' };
+  public searchTerm: string = "";
 
-  constructor(private bookService: BookService, 
-    private categoryService: CategoryService,  
+  constructor(private bookService: BookService,
+    private categoryService: CategoryService,
     private authService: AuthService,
-    private router: Router, private route:ActivatedRoute) {   // get the current user from authentication service
-    this.authService.currentUser.subscribe(x => this.currentUser= x);}
+    private router: Router, private route: ActivatedRoute) {   // get the current user from authentication service
+    this.authService.currentUser.subscribe(x => this.currentUser = x);
+  }
   ngOnInit(): void {
     // this.categoryService.getAllCategories()
     // .subscribe(c => this.categories = c);
-    
+
     this.categoryService.getCategoriesWithoutBooks().subscribe(x => this.categories = x);
 
     this.route.params.subscribe(params => {
-      if(params['searchTerm'])
-      this.searchTerm=params['searchTerm'];
+      if (params['searchTerm'])
+        this.searchTerm = params['searchTerm'];
     })
   }
   // search():void{
@@ -47,63 +48,64 @@ export class AppComponent {
   //   this.router.navigate(['/Book', this.searchTerm]);
   // }
 
- showSearch(): void {
-
+  /* showSearch(): void {
+ 
+     if (this.filterTerm == null || this.filterTerm == '') {
+      alert("The input field is empty")
+     }
+ 
+     else if (this.filterTerm.length >= 0 ){
+       this.bookService.getAllBooks()
+     .subscribe(p => this.allBooks = p);
+     console.log(this.allBooks);
+     this.router.navigate(['/Book', this.filterTerm]);
+     }
+ 
+ 
+   } */
+  click() {
     if (this.filterTerm == null || this.filterTerm == '') {
-     alert("The input field is empty")
+      alert("The input field is empty")
     }
 
-    else if (this.filterTerm.length >= 0 ){
+    else if (this.filterTerm.length >= 0) {
       this.bookService.getAllBooks()
-    .subscribe(p => this.allBooks = p);
-    console.log(this.allBooks);
-    this.router.navigate(['/Book', this.filterTerm]);
-    }
+        .subscribe(p => this.allBooks = p);
+      console.log(this.allBooks);
 
+      this.bookService.search.next(this.filterTerm);
+      this.router.navigate(['/Book', this.filterTerm]);
+    }
+  }
+  checkSearch(event: any) {
+    if (event.key === "Enter" || this.filterTerm == null) {
+      this.allBooks = [];
+      this.searchTerm = (event.target as HTMLInputElement).value;
+      console.log(event);
+      this.bookService.search.next(this.filterTerm);
+      this.router.navigate(['/Book', this.filterTerm]);
+    }
+  }
+
+  logout() {
+    if (confirm('Are you sure you want to log out?')) {
+      // ask authentication service to perform logout
+      this.authService.logout();
+
+
+      // subscribe to the changes in currentUser, and load Home component
+      this.authService.currentUser.subscribe(x => {
+        this.currentUser = x
+        this.router.navigate(['/']);
+      });
+    }
 
   }
-  click(){
-      if (this.filterTerm == null || this.filterTerm == '') {
-        alert("The input field is empty")
-       }
 
-       else if (this.filterTerm.length >= 0 ){
-         this.bookService.getAllBooks()
-       .subscribe(p => this.allBooks = p);
-       console.log(this.allBooks);
-       
-       
-       this.router.navigate(['/Book', this.filterTerm]);
-       }
-    }
-    checkSearch(event:any){
-      if (event.key === "Enter" || this.filterTerm == null) {
-        this.allBooks = [];
-        this.searchTerm=(event.target as HTMLInputElement).value;
-        console.log(event);
-        this.bookService.search.next(this.filterTerm);
-      }
-    }
- 
-    logout() {
-      if (confirm('Are you sure you want to log out?')) {
-        // ask authentication service to perform logout
-        this.authService.logout();
-        
-  
-        // subscribe to the changes in currentUser, and load Home component
-        this.authService.currentUser.subscribe(x => {
-          this.currentUser = x
-          this.router.navigate(['/']);
-        });
-      }
-  
-    }
-
-//   pageYoffset = 0;
-//   @HostListener('window:scroll', ['$event']) onScroll(event){
-//     this.pageYoffset = window.pageYOffset;
-//  }
+  //   pageYoffset = 0;
+  //   @HostListener('window:scroll', ['$event']) onScroll(event){
+  //     this.pageYoffset = window.pageYOffset;
+  //  }
   // @ViewChild('scroll')
   // scroll!: ElementRef;
 
@@ -111,22 +113,22 @@ export class AppComponent {
 
 
 
-  
-
- /* scrollToTop(){
-  // this.scroll.scrollToPosition([0,0]);
-  this.scroll.nativeElement.scrollToTop = 0;
-}
-
-scrollToBottom(){
-  console.log(this.scroll.nativeElement.scrollHeight)
-  this.scroll.nativeElement.scrollToTop = this.scroll.nativeElement.scrollHeight;
-} */
 
 
-// searchKey(event:any){
-//   this.searchTerm=(event.target as HTMLInputElement).value;
-//   console.log(this.searchTerm);
-//   this.bookService.search.next(this.searchTerm);
-// } 
+  /* scrollToTop(){
+   // this.scroll.scrollToPosition([0,0]);
+   this.scroll.nativeElement.scrollToTop = 0;
+ }
+ 
+ scrollToBottom(){
+   console.log(this.scroll.nativeElement.scrollHeight)
+   this.scroll.nativeElement.scrollToTop = this.scroll.nativeElement.scrollHeight;
+ } */
+
+
+  // searchKey(event:any){
+  //   this.searchTerm=(event.target as HTMLInputElement).value;
+  //   console.log(this.searchTerm);
+  //   this.bookService.search.next(this.searchTerm);
+  // } 
 }
