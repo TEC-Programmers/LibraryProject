d3d4ca367a90d57f12edb6fa4d68ea6849017fce
Afrinarahaman <70579511+Afrinarahaman@users.noreply.github.com>
2022-07-08 13:08:26 +0200

    changes

--- LibraryProject.Client/src/app/app.component.ts
+++ LibraryProject.Client/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { User } from './_models/User';
 import { AuthService } from './_services/auth.service';
 import { BookService } from './_services/book.service';
 import { CategoryService } from './_services/category.service';
+import { UserService } from './_services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -24,24 +25,78 @@ export class AppComponent {
   searchBooks: Book[] = [];
   currentUser: User = { id: 0, firstName: '', middleName: '', lastName: '', email: '', password: '', role: 0 };
   public searchTerm: string = "";
+  x:any;
+ 
 
   constructor(private bookService: BookService,
     private categoryService: CategoryService,
     private authService: AuthService,
-    private router: Router, private route: ActivatedRoute) {   // get the current user from authentication service
+    private router: Router, private route: ActivatedRoute,
+    private userService: UserService) {   // get the current user from authentication service
     this.authService.currentUser.subscribe(x => this.currentUser = x);
+    // console.log('user role: ',this.currentUser.role)
   }
+
+  logout() {
+    if (confirm('Are you sure you want to log out?')) {
+      this.userService.getRole_(0);      
+      // ask authentication service to perform logout
+      this.authService.logout();
+
+      // subscribe to the changes in currentUser, and load Home component
+      this.authService.currentUser.subscribe(x => {
+        this.currentUser = x;
+        this.router.navigate(['Login']);
+      });
+    }
+    else {
+      if (this.x === 1) {
+        this.router.navigate(['Admin']);
+      }
+      else {
+        this.router.navigate(['Frontpage']);
+      }
+    }
+  }
+
+
   ngOnInit(): void {
     // this.categoryService.getAllCategories()
     // .subscribe(c => this.categories = c);
 
-    this.categoryService.getCategoriesWithoutBooks().subscribe(x => this.categories = x);
+      this.categoryService.getCategoriesWithoutBooks().subscribe(x => this.categories = x);
+      this.showOrhideAdminBtn();
 
     this.route.params.subscribe(params => {
       if (params['searchTerm'])
         this.searchTerm = params['searchTerm'];
     })
-  }
+    }
+
+
+    ngOnDestroy() {
+        this.userService.getRole.unsubscribe();
+    }
+
+
+    showOrhideAdminBtn() {
+        this.authService.currentUser.subscribe(x => {
+            this.currentUser = x;
+
+            if (this.currentUser) {
+                if (this.currentUser.role.toString() === 'Administrator') {
+                    this.userService.getRole$.subscribe(x => this.x = x); // start listening for changes 
+                }
+                else {
+                    this.userService.getRole_(0);
+                }
+            }
+            else {
+                this.userService.getRole_(0);
+            }
+        });
+    }
+
   // search():void{
 
   //   if(this.searchTerm)
@@ -86,7 +141,7 @@ export class AppComponent {
       this.router.navigate(['/Book', this.filterTerm]);
     }
   }
-
+/* 
   logout() {
     if (confirm('Are you sure you want to log out?')) {
       // ask authentication service to perform logout
@@ -100,7 +155,7 @@ export class AppComponent {
       });
     }
 
-  }
+  } */
 
   //   pageYoffset = 0;
   //   @HostListener('window:scroll', ['$event']) onScroll(event){
