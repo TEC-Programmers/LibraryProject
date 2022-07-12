b6039d2614df926b89718cdc4a6adad45acc1312
Rizwanah Mustafa <rizw0048@elev.tec.dk>
2022-07-12 14:53:13 +0200

    Frontpage designing

--- LibraryProject.Client/src/app/frontpage/frontpage.component.ts
+++ LibraryProject.Client/src/app/frontpage/frontpage.component.ts
@@ -11,6 +11,8 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { AuthService } from 'app/_services/auth.service';
 import { UserService } from 'app/_services/user.service';
 import { User } from 'app/_models/User';
+import { CategoryService } from '../_services/category.service';
+import { Category } from 'app/_models/Category';
 
 
 @Component({
@@ -19,10 +21,18 @@ import { User } from 'app/_models/User';
   styleUrls: ['./frontpage.component.css']
 })
 export class FrontpageComponent implements OnInit {
+  title = 'LibraryProject-Client';
+
+  book!: Book;
+  counter = 0;
+  total: number = 0;
+  categories: Category[] = [];
+  allBooks: Book[] = [];
+  filterTerm!: string;
   currentUser: User ={ id: 0, firstName: '', middleName: '', lastName: '', email: '', password: '', role: 0};
   x:any;
 
-  book: Book = {
+  newbook: Book = {
     id: 0, title: "", publishYear: 0, description: "", image: "", publisherId: 0, categoryId: 0,
     language: '',
     authorId: 0,
@@ -38,25 +48,32 @@ export class FrontpageComponent implements OnInit {
   }
   books: Book[] = [];
   bookId: number = 0;
-  public filterTerm: string = "";
+  public searchTerm: string = "";
   searchBooks: Book[] = [];
 
 
 
 
 
-    constructor(private bookService: BookService, private route: ActivatedRoute, private router: Router, private ref: ChangeDetectorRef, private authService: AuthService, private userService: UserService) {
+    constructor(private bookService: BookService,
+      private categoryService: CategoryService,  
+       private route: ActivatedRoute, 
+       private router: Router,
+        private ref: ChangeDetectorRef,
+         private authService: AuthService, 
+         private userService: UserService) {
 
   }
 
   ngOnInit(): void {
+    
     this.route.params.subscribe(params => {
       if (params['filterTerm']) {
         this.bookService.getAllBooks().subscribe(x => {
           this.books = x;
           this.searchBooks = this.books;
 
-
+          this.categoryService.getCategoriesWithoutBooks().subscribe(x => this.categories = x);
           this.bookService.search.subscribe((value: string) => {
 
             this.filterTerm = value
@@ -77,36 +94,46 @@ export class FrontpageComponent implements OnInit {
     })
 
   }
+  showSearch(): void {
 
-
-  showOrhideAdminBtn() {
-    this.authService.currentUser.subscribe(user => {
-    this.currentUser = user;
-  
-    if (this.x !== 1) {
-      if (this.currentUser) {
-        this.userService.getRole$.subscribe(x => this.x = x); // start listening for changes 
-          if (this.currentUser.role.toString() === 'Administrator') {
-            this.userService.getRole_(1);
-          }
-          else {
-            this.userService.getRole_(0);
-          }
-        }
-        else {
-          this.userService.getRole_(0);
-        } 
+    if (this.filterTerm == null || this.filterTerm == '') {
+     alert("The input field is empty")
     }
-    });
+
+    else if (this.filterTerm.length >= 0 ){
+      this.bookService.getAllBooks()
+    .subscribe(p => this.allBooks = p);
+    console.log(this.allBooks);
+    }
+  }
+  click() {
+    if (this.filterTerm == null || this.filterTerm == '') {
+      alert("The input field is empty")
+    }
+
+    else if (this.filterTerm.length >= 0) {
+      this.bookService.getAllBooks()
+        .subscribe(p => this.allBooks = p);
+      console.log(this.allBooks);
+
+      this.bookService.search.next(this.filterTerm);
+      this.router.navigate(['/Book', this.filterTerm]);
+    }
+  }
+  checkSearch(event: any) {
+    if (event.key === "Enter" || this.filterTerm == null) {
+      this.allBooks = [];
+      this.searchTerm = (event.target as HTMLInputElement).value;
+      console.log(event);
+      this.bookService.search.next(this.filterTerm);
+      this.router.navigate(['/Book', this.filterTerm]);
+    }
   }
 
+ 
 
 
 
-  
-  ngAfterContentChecked(): void {
-    this.ref.detectChanges();
-    this.showOrhideAdminBtn();  
-  }
+
 
 }
